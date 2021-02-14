bfd55267c7912943a41d03b747899525b91d49f8
David Armendariz <darmendariz1998@outlook.com>
2021-02-14 00:19:11 -0500

    Made changes to conftest.py

--- src/conftest.py
+++ src/conftest.py
@@ -2,13 +2,11 @@ import os
 import pytest
 from django.conf import settings
 
-DEFAULT_ENGINE = "django.db.backends.postgresql_psycopg2"
-
 
 @pytest.fixture(scope="session")
 def django_db_setup():
     settings.DATABASES["default"] = {
-        "ENGINE": os.environ.get("DB_TEST_ENGINE", DEFAULT_ENGINE),
+        "ENGINE": os.environ["DB_TEST_ENGINE"],
         "HOST": os.environ["DB_TEST_HOST"],
         "NAME": os.environ["DB_TEST_DATABASE"],
         "PORT": os.environ["DB_TEST_PORT"],
